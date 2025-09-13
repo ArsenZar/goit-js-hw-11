1d0bee5ce9ad9d55f639811e53d62b14619152d0
ArsenZar <arsenzar@gmail.com>
2025-09-13 16:04:40 +0300

    first step ready

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import axios from "axios";
-// Описаний у документації
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-const myApiKey = "52255318-c301db47a64feee351027f4f6";
+import getImagesByQuery from "./js/pixabay-api";
 
+const form = document.querySelector(".form");
+const input = document.querySelector(".formInput");
 
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
-axios.get("https://pixabay.com/api/?key=52255318-c301db47a64feee351027f4f6&q=yellow+flowers&image_type=photo")
-.then(res => console.log(res.data)
-);
+  getImagesByQuery(input.value)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+});
